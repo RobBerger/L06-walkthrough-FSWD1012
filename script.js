53be111dd37b50ddf60e47c30dcbc7aed53a8432
berger <robertberger.dev@gmail.com>
2023-01-02 11:26:12 -0500

    added final example code

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ function getJoke(){
         'https://backend-omega-seven.vercel.app/api/getjoke',
         {
             success: function (APIResponse) {
-                console.log(APIResponse)
+                console.log(APIResponse);
                 let joke = JSON.parse(APIResponse);
-                console.log(joke)
+                console.log(joke);
                 let myJokeDiv = document.createElement('div');
                 myJokeDiv.innerHTML = joke[0].question;
                 let myButton = document.createElement('button');
-                myButton.innerHTML = "Get Punchline";
+                myButton.innerHTML = "Get Punchline"
+                myButton.onclick = function(){
+                    myJokeDiv.innerHTML += " " + joke[0].punchline
+                }
+                document.body.appendChild(myJokeDiv);
+                document.body.appendChild(myButton);
             }
         })
 }
